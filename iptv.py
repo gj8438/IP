7457435ad590d0b0e4e110fa3f9062dd61a470d4
mlyuanlang <159904851+mlyuanlang@users.noreply.github.com>
2024-02-16 04:33:55 +0800

    Update iptv.py

--- iptv.py
+++ iptv.py
@@ -249,6 +249,10 @@ with open("iptv.txt", 'w', encoding='utf-8') as file:
     for result in results:
         file.write(result + "\n")
         print(result)
+        
+# 对频道进行排序
+results.sort(key=lambda x: (x[0], -float(x[2].split()[0])))
+results.sort(key=lambda x: channel_key(x[0]))
 
         
 time.sleep(10)
