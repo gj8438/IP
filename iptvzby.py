40faea89b3ddd6500b1d9990ed8fecd731e3ee4e
gj8438 <67566398+gj8438@users.noreply.github.com>
2024-03-01 07:06:52 +0800

    Update iptvzby.py

--- iptvzby.py
+++ iptvzby.py
@@ -354,7 +354,7 @@ def worker():
 
 
 # 创建多个工作线程
-num_threads = 20
+num_threads = 10
 for _ in range(num_threads):
     t = threading.Thread(target=worker, daemon=True)
     # t = threading.Thread(target=worker, args=(event,len(channels)))  # 将工作线程设置为守护线程
@@ -485,7 +485,7 @@ def worker():
 
 
 # 创建多个工作线程
-num_threads = 20
+num_threads = 10
 for _ in range(num_threads):
     t = threading.Thread(target=worker, daemon=True)
     # t = threading.Thread(target=worker, args=(event,len(channels)))  # 将工作线程设置为守护线程
